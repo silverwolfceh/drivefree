f05117371dc29bdbd1402bb806bc0f0ec1bc9275
Tong Vuu <tong.vuu@terralogic.com>
2019-04-06 16:24:52 +0700

    Heroku specified env

--- index.php
+++ index.php
@@ -1,15 +1,31 @@
-
-<?php error_reporting(~E_ALL); ?>
 <?php include_once 'header.php'; ?>
-
 <div class="container">
+<?php
+	$list_api = array();
+	$num_acc = getenv('NUM_ACC');
+	if(int($num_acc) > 0)
+	{
+		for($i = 0; $i < $num_acc; $i++)
+		{
+			$idx = $i + 1;
+			$c = getenv("ACC_".$idx);
+			$api_name = "v_".$idx.".json";
+			$f = fopen($api_name, "w");
+			$list_api["ACC_".$idx] = $api_name;
+			fwrite($f, $c);
+			fclose($f);
+		}
+	} 
+?>
 
 <form class="form-inline" action="view.php" method="post">
 	<div class="form-group">
 	<select class="form-control" name="sverApi">
-		<option value="v1.json">Google Drive V1</option>
-		<option value="v2.json">Google Drive V2</option>
-		<option value="v3.json">Google Drive V3</option>
+		<?php
+			foreach ($list_api as $name => $api) {
+				echo "<option value='".$api."'>".$name."</option>"
+			}
+		?>
 	</select>
 	<input class="btn btn-success" type="submit" value="Chọn">
 </div>
